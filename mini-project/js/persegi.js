c8b6e91c2144a2c6e6cef26ba89426bdf74deb40
admiral215 <101520167+admiral215@users.noreply.github.com>
2024-01-17 20:20:01 +0700

    Add files via upload

---
+++ mini-project/js/persegi.js
@@ -0,0 +1,13 @@
+function luas(){
+    var s = document.getElementById('s').value ;
+    var luas = s*s;
+
+    document.getElementById('luas').innerHTML = luas;
+}
+
+function kel(){
+    var sk = document.getElementById('sk').value ;
+    var kel = 4 * sk;
+
+    document.getElementById('kel').innerHTML = kel;
+}
